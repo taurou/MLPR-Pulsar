c7e4f5b75ad06d6aa29a7b698271426d1f12ce28
taurou <80039469+taurou@users.noreply.github.com>
2023-01-17 12:05:37 +0100

    Implemented MVG backbone

---
+++ function_lib/MVG.py
@@ -0,0 +1,106 @@
+import Lab4 as LAB4
+import Lab3_LDA as LDA
+import numpy as np
+import matplotlib.pyplot as plt
+import lib
+import scipy.special
+import math
+
+
+def KfoldCrossValidation(D, L, k, prior, classifierFunction, seed=0): #passing the classifier function
+    np.random.seed(seed)
+    idx = np.random.permutation(D.shape[1]) #randomizes the order of the data
+
+    group_size = int(math.ceil((D.shape[1]/k)))
+    goodEstimation = 0
+    totalEstimation = 0
+
+    for i in range(k):
+        idxTest = idx[i*group_size:(i*group_size + group_size)]
+        idxTrain = [i for i in idx if i not in set(idxTest)]
+        predicted_L = classifierFunction(D[:,idxTrain], L[idxTrain], D[:,idxTest], L[idxTest], prior)
+        BooleanPredictionResults = predicted_L == L[idxTest]
+        goodEstimation += sum(BooleanPredictionResults)
+        totalEstimation += len(predicted_L)
+    
+    Accuracy = goodEstimation / totalEstimation
+    Error = 1 - Accuracy
+    print("KFOLD Accuracy: ", Accuracy, "Error: ", Error)   #TODO correct rounding 
+
+
+def LeaveOneOutCrossValidation(D, L, prior, classifierFunction, seed=0):
+    KfoldCrossValidation(D, L, D.shape[1], prior, classifierFunction, seed)
+
+
+#if diagonal is set to true, only the diagonal will be kept and the other elements will be zeroed
+
+def compute_Mean_CovMatr_perClass(D, L, diagonal=False): #put diagonal = true optionally if we want to use just the diagonal of the cov matrix
+    mean_cov_class = {}
+    # generating a diagonal matrix with ones with numClass * numClass dimension
+    for i in set(list(L)):  # potrebbero essere anche parole...
+        (mu, C) = LAB4.compute_Mean_CovMatr(D[:, L == i])
+        if (diagonal):
+            C = np.diag(np.diag(C))
+        mean_cov_class[i] = (mu, C)
+    return mean_cov_class
+
+#takes as input the dictionary label : (mu, C) and the data.
+
+def compute_logDensity_AllSamples(D, mu_C_tuple):
+    matrix = []
+    (mu, c) = mu_C_tuple
+    matrix.append(LAB4.logpdf_GAU_ND(D, mu, c))
+    return np.array(matrix)
+
+
+def MVG_basePredictor(eval_D, eval_L, mu_C, prior): #TODO correct eval_L ! It's never used!!
+
+    logDens = np.zeros((len(mu_C), eval_D.shape[1]))
+    logSJoint = np.zeros((len(mu_C), eval_D.shape[1]))
+    
+    for label, mean_covmatr in mu_C.items():
+        logDens[label, :] = compute_logDensity_AllSamples(eval_D, mean_covmatr)
+        logSJoint[label, :] =logDens[label, :] + np.log(prior[label, :])
+
+    logSMarginal = lib.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    CPosterior = np.exp(logSPost)
+
+    llr = logDens[1, :] - logDens[0, :] #compute llr between class 1 and class 0.
+    # Computes the LABEL with max Class-Posterior-Probability
+    predicted_labels = np.argmax(CPosterior, axis=0)
+    return predicted_labels, llr #return the predicted labels 
+
+    
+def TiedCovariance_logMVG(train_D, train_L, eval_D, eval_L, prior):
+    _, Sw = LDA.computeSbSw(train_D, train_L) #computing the empirical within covariance matrix
+    class_means = lib.class_mean_vector(train_D, train_L)
+    mu_C = { i:(lib.vcol(class_means[:,i:i+1]),Sw) for i in set(list(train_L))} #formatting the right input for the base function
+    return MVG_basePredictor(eval_D, eval_L, mu_C, prior)
+
+def TiedCovariance_logNB(train_D, train_L, eval_D, eval_L, prior):
+    _, Sw = LDA.computeSbSw(train_D, train_L) #computing the empirical within covariance matrix
+    Sw = np.diag(np.diag(Sw))
+    class_means = lib.class_mean_vector(train_D, train_L)
+    mu_C = { i:(lib.vcol(class_means[:,i:i+1]),Sw) for i in set(list(train_L))} #formatting the right input for the base function
+    return MVG_basePredictor(eval_D, eval_L, mu_C, prior)
+
+def logMVG(train_D, train_L, eval_D, eval_L, prior):
+    mu_C = compute_Mean_CovMatr_perClass(train_D, train_L)
+    return MVG_basePredictor(eval_D, eval_L, mu_C, prior)
+
+def logNaiveBayes(train_D, train_L, eval_D, eval_L, prior):
+    mu_C = compute_Mean_CovMatr_perClass(train_D, train_L, diagonal=True)
+    return MVG_basePredictor(eval_D, eval_L, mu_C, prior)
+
+
+if __name__ == '__main__':
+    D, L = lib.load_iris()
+    (train_D, train_L), (eval_D, eval_L) = lib.split_db_2to1(D, L)
+
+    prior = lib.vcol(np.array([1.0/3.0, 1.0/3.0, 1.0/3.0]))
+    logMVG(train_D, train_L, eval_D, eval_L, prior)
+    logNaiveBayes(train_D, train_L, eval_D, eval_L, prior)
+    TiedCovariance_logMVG(train_D, train_L, eval_D, eval_L, prior)
+    TiedCovariance_logNB(train_D, train_L, eval_D, eval_L, prior)
+
