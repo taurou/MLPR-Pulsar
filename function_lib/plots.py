a231ded4c9e60c201802c10c972e2a6dd7de2bb1
taurou <80039469+taurou@users.noreply.github.com>
2023-01-22 18:32:46 +0100

    high-res plots

--- function_lib/plots.py
+++ function_lib/plots.py
@@ -29,7 +29,7 @@ def plot_hist(D, L, title = " ", filename = "defaultname", save = False ):
         plt.legend()
         plt.tight_layout() # Use with non-default font size to keep axis label inside the figure
         if(save):        
-            plt.savefig('%s_hist_%d-stepfilled.png' % (filename, dIdx) )
+            plt.savefig('%s_hist_%d-stepfilled.png' % (filename, dIdx), dpi=250 )
     plt.show()
 
 def heatmap(D, L, title = " ", filename = "defaultname", save = False ):
@@ -41,7 +41,7 @@ def heatmap(D, L, title = " ", filename = "defaultname", save = False ):
         plt.title(title + " - " + labels[i])
         plt.imshow(corrcoeffs[i], cmap=map_colours[i], interpolation='nearest')
         if(save):        
-            plt.savefig('%s_heatmap_%s.png' % (filename, labels[i]) )
+            plt.savefig('%s_heatmap_%s.png' % (filename, labels[i]), dpi=250 )
     plt.show()
 
 def plotminDCF(x,minDCF_array,prior_t, x_label, filename = "minDCF",  save = False, logScale = True):
@@ -56,39 +56,13 @@ def plotminDCF(x,minDCF_array,prior_t, x_label, filename = "minDCF",  save = Fal
     plt.ylabel("minDCF")
     plt.xlim([x[0], x[len(x)-1]])
     for idx, pi_t in enumerate(prior_t):
-        labelDCF = "minDCF pi = %.1f" % (pi_t)    
+        labelDCF = "minDCF π=%.1f" % (pi_t)    
         plt.plot(x, minDCF_array[idx], label=labelDCF, color=colours[idx])
         
-    plt.legend([ "minDCF pi=%.1f" % (prior) for prior in prior_t ])
+    plt.legend([ "minDCF π=%.1f" % (prior) for prior in prior_t ])
     if(save):        
-        plt.savefig('%s.png' % (filename) )
+        plt.savefig('%s.png' % (filename), dpi=250 )
         plt.close()
     else:
         plt.show()
 
-
-def old_plotminDCF(x,minDCF_array,prior_t, x_label, unbalanced_minDCF = None, filename = "minDCF", save = False):
-    
-    colours=['r','y','b']
-
-    for prioridx in prior_t:
-
-        plt.figure()
-        plt.xscale("log")
-        plt.xlabel(x_label)
-        plt.ylabel("minDCF")
-        plt.xlim([x[0], x[len(x)-1]])
-        for idx, pi_t in enumerate(prior_t):
-            labelDCF = "minDCF pi = %.1f" % (pi_t)    
-            plt.plot(x, np.vstack(minDCF_array)[:,idx], label=labelDCF, color=colours[idx])
-        
-        if unbalanced_minDCF is not None:    
-            plt.plot(x, unbalanced_minDCF, label="minDCF unbalanced", color='g')
-            plt.legend([ "minDCF pi=%.1f" % (prior) for prior in prior_t].append("minDCF unbalanced"))
-        else:
-            plt.legend([ "minDCF pi=%.1f" % (prior) for prior in prior_t ])
-        
-        if(save):        
-            plt.savefig('%s_%s.png' % (filename, x_label) )
-
-        plt.close()
